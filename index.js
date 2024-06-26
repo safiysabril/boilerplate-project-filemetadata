70f1d864e01ca84052f4d7df68a15c0098d15bf3
Muhammad Safiy <safiysabril@gmail.com>
2024-06-26 13:48:45 +0800

    final project - filemetadata microservice

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +13,31 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Multer storage configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // File renaming logic (here, timestamp + original extension)
+  }
+});
 
+const upload = multer({ storage: storage });
 
+// Handle file upload POST request
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  // Return file details
+  res.json({
+    filename: req.file.filename,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
